2e8642431fb9d2e91d80dba7ec3c87f5e3475146
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2022-10-19 19:37:43 +0530

    correction added

--- prime_01.js
+++ prime_01.js
@@ -1,11 +1,12 @@
 let n=13;
 
-count=0;
+let count=0;
 
 for(let i=0;i<=n-1;i++){
 
-if(n%i==0)
-count++;
-
-count>2 ? console.log("isprime") : console.log("Not a prime no.)
+    if(n%i==0){
+      count++;
+     }
 }
+count==2 ? console.log("isprime") : console.log("Not a prime no.);
+
